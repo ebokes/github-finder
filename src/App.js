6a50a095abcd99e7d491ad455aabf292cf9fd2bd
Buokem <talk2chibu@gmail.com>
2021-09-28 20:14:51 +0100

    Creating events, passing props and clearing user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search  from './components/users/Search';
 
 class App extends Component {
   state={
@@ -10,18 +11,37 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount(){
+  // async componentDidMount(){
+  //   this.setState({ loading: true })
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //   this.setState({ users: res.data.item, loading: false })
+  // }
+  
+  searchUsers = async text => {
     this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    this.setState({ users: res.data, loading: false })
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ users: res.data.items, loading: false })
   }
+  //clear users from state 
+  clearUsers = () => this.setState({users: [], loading: false})
 
   render(){
+    const {users, loading} = this.state
     return (
       <div className="App">
         <Navbar />
+        <Search 
+          searchUsers={this.searchUsers} 
+          clearUsers={this.clearUsers} 
+          showClear={users.length > 0 ? true: false}  
+        />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Users 
+            loading={loading} 
+            users={users}
+          />
         </div>
       </div>
     );
